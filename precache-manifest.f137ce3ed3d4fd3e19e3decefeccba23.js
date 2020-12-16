3a8fa2602245848f73f2d59dd0bb266c46190df0
Aditya Kumar <adipan43@gmail.com>
2020-12-17 00:29:48 +0530

    Updates

---
+++ precache-manifest.f137ce3ed3d4fd3e19e3decefeccba23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53abaed447b213b5a6f1bd75059a384d",
+    "url": "/recipe-react/index.html"
+  },
+  {
+    "revision": "9dacd5acc9c2519b86e1",
+    "url": "/recipe-react/static/css/main.e163401f.chunk.css"
+  },
+  {
+    "revision": "12d7401412c59ceacba5",
+    "url": "/recipe-react/static/js/2.361cbb09.chunk.js"
+  },
+  {
+    "revision": "9dacd5acc9c2519b86e1",
+    "url": "/recipe-react/static/js/main.b7e7f415.chunk.js"
+  },
+  {
+    "revision": "665fcce085e729b26044",
+    "url": "/recipe-react/static/js/runtime~main.f54b8cf1.js"
+  }
+]);
